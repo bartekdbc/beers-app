edebd30ad701c9d5b59df60460fa31cef6b55306
Bartlomiej Debiec <116588815+bartekdbc@users.noreply.github.com>
2023-03-29 15:13:38 +0700

    BeerList component styles updated and rhombus icon implemented

--- src/components/BeerList/index.js
+++ src/components/BeerList/index.js
@@ -1,11 +1,14 @@
-import { Image, Tagline, Title, Wrapper } from "./styled";
+import { Icon, Image, Info, Tagline, Title, Wrapper } from "./styled";
 
 const BeerList = ({ image, name, tagline }) => {
   return (
     <Wrapper>
-      <Image src={image} alt="" />
-      <Title>{name}</Title>
-      <Tagline>{tagline}</Tagline>
+      <Image src={image} alt="Beer Photo" />
+      <Info>
+        <Title>{name}</Title>
+        <Icon />
+        <Tagline>{tagline}</Tagline>
+      </Info>
     </Wrapper>
   );
 };
--- src/components/BeerList/styled.js
+++ src/components/BeerList/styled.js
@@ -1,14 +1,13 @@
 import styled from "styled-components";
+import { ReactComponent as Rhombus } from "./rhombus.svg";
 
 export const Wrapper = styled.div`
   border: 1px solid #eae8e8;
-  padding: 30px 20px;
+  padding: 20px 20px 5px 20px;
   margin: 20px;
-  display: flex;
-  flex-direction: column;
+  display: grid;
+  grid-template-rows: minmax(110px, 1fr) 1fr;
   justify-content: center;
-  align-items: center;
-  text-align: center;
 `;
 
 export const Image = styled.img`
@@ -17,11 +16,20 @@ export const Image = styled.img`
   margin: auto;
 `;
 
+export const Info = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+`;
+
 export const Title = styled.h2`
   font-size: 14px;
   color: #9a9a9a;
   margin: 0;
   padding: 10px;
+  text-size-adjust: auto;
 `;
 
 export const Tagline = styled.h3`
@@ -30,4 +38,11 @@ export const Tagline = styled.h3`
   margin: 0;
   padding: 10px 0;
   border-top: 1px solid #eae8e8;
+`;
+
+export const Icon = styled(Rhombus)`
+  width: 10px;
+  height: 10px;
+  margin-bottom: -5px;
+  position: sticky;
 `;
