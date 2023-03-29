f08d9cbdde37fe2ed1e0146bbe6f3bf0186ac1bf
Bartlomiej Debiec <116588815+bartekdbc@users.noreply.github.com>
2023-03-29 23:35:30 +0700

    tanstack query devtools added to app and defaultOptions set

--- src/index.js
+++ src/index.js
@@ -2,11 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { GlobalStyle } from "./assets/GlobalStyle";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { fetchBeers } from "./beersAPI";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 10, // 10 seconds
+      cacheTime: 1000 * 10, // 10 seconds
+      queryFn: fetchBeers,
+    },
+  },
+});
+
 root.render(
-  <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools />
     <GlobalStyle />
     <App />
-  </React.StrictMode>
+  </QueryClientProvider>
 );
