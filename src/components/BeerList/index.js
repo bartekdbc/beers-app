f9a65a459d4cfd29170cc543417cb41432137cb7
Bartlomiej Debiec <116588815+bartekdbc@users.noreply.github.com>
2023-03-29 20:30:35 +0700

    BeerList refactored

--- src/components/BeerList/index.js
+++ src/components/BeerList/index.js
@@ -1,15 +1,55 @@
-import { Icon, Image, Info, Tagline, Title, Wrapper } from "./styled";
+import { useEffect, useState } from "react";
+import BeerTile from "../BeerTile";
+import { Container } from "../Container";
+import Loader from "../Loader";
+import Pagination from "../Pagination";
 
-const BeerList = ({ image, name, tagline }) => {
+const BeerList = () => {
+  const [beers, setBeers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const fetchData = async (currentPage) => {
+    setLoading(false);
+    const response = await fetch(
+      `https://api.punkapi.com/v2/beers?page=1&per_page=12`
+    );
+    const data = await response.json();
+    setBeers(data);
+    console.log(data);
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(fetchData, 2000);
+  }, [currentPage]);
+
   return (
-    <Wrapper>
-      <Image src={image} alt="Beer Photo" />
-      <Info>
-        <Title>{name}</Title>
-        <Icon />
-        <Tagline>{tagline}</Tagline>
-      </Info>
-    </Wrapper>
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <Container>
+            {beers.map((beer) => (
+              <BeerTile
+                key={beer.id}
+                image={beer.image_url}
+                name={beer.name}
+                tagline={beer.tagline}
+                description={beer.description}
+                abv={beer.abv}
+                ibu={beer.ibu}
+              />
+            ))}
+          </Container>
+          <Pagination
+            currentPage={currentPage}
+            setCurrentPage={setCurrentPage}
+          />
+        </>
+      )}
+    </>
   );
 };
 
