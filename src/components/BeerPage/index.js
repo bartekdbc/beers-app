2d31b9bbfc7a63027f2ffe69a057d977d0533d70
Bartlomiej Debiec <116588815+bartekdbc@users.noreply.github.com>
2023-03-30 19:30:53 +0700

    BeerPage component implemented to App

--- src/components/BeerPage/index.js
+++ src/components/BeerPage/index.js
@@ -1,5 +1,81 @@
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
+import { fetchBeerDetails } from "../../beersAPI";
+import ErrorPage from "../ErrorPage";
+import Loader from "../Loader";
+import PausedPage from "../PausedPage";
+import Ingredients from "./Ingredients";
+import {
+  Description,
+  DetailedInfo,
+  DetailedInfoWrapper,
+  Details,
+  Image,
+  Info,
+  IngredientsWrapper,
+  Name,
+  SmallInfo,
+  Strong,
+  Tagline,
+  Wrapper,
+} from "./styled";
+
 const BeerPage = () => {
-  return <></>;
+  const { id } = useParams();
+
+  const {
+    data: beer,
+    isLoading,
+    isError,
+    isPaused,
+  } = useQuery(["beer", { id }], () => fetchBeerDetails(id));
+
+  return (
+    <>
+      {beer ? (
+        <>
+          <Wrapper>
+            <Details>
+              <Image src={beer[0].image_url} alt="Beer Pic" />
+              <Info>
+                <Name>{beer[0].name}</Name>
+                <Tagline>{beer[0].tagline}</Tagline>
+                <span>
+                  <Strong>Description:</Strong>
+                  <Description>{beer[0].description}</Description>
+                </span>
+                <DetailedInfo>
+                  <DetailedInfoWrapper>
+                    <Strong>ABV:</Strong>
+                    <SmallInfo>{beer[0].abv}</SmallInfo>
+                  </DetailedInfoWrapper>
+                  <DetailedInfoWrapper>
+                    <Strong>IBU:</Strong>
+                    <SmallInfo>{beer[0].ibu}</SmallInfo>
+                  </DetailedInfoWrapper>
+                </DetailedInfo>
+              </Info>
+            </Details>
+            <IngredientsWrapper>
+              <Ingredients
+                malt={beer[0].ingredients.malt}
+                hops={beer[0].ingredients.hops}
+                yeast={beer[0].ingredients.yeast}
+              />
+            </IngredientsWrapper>
+          </Wrapper>
+        </>
+      ) : isPaused ? (
+        <PausedPage />
+      ) : isLoading ? (
+        <Loader />
+      ) : isError ? (
+        <ErrorPage />
+      ) : (
+        <></>
+      )}
+    </>
+  );
 };
 
 export default BeerPage;
---
+++ src/components/BeerPage/styled.js
@@ -0,0 +1,69 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  margin: 0 auto;
+  max-width: 800px;
+  min-height: 100vh;
+`;
+
+export const Details = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border: 1px solid #eae8e8;
+  padding: 20px 50px;
+  margin-top: 50px;
+  box-shadow: rgba(186, 199, 213, 0.5) 0px 4px 12px;
+`;
+
+export const Image = styled.img`
+  max-height: 300px;
+  margin: auto;
+  object-fit: cover;
+  margin: 10px 40px;
+`;
+
+export const Info = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Name = styled.h2`
+  margin: 0;
+  color: black;
+`;
+
+export const Tagline = styled.h3`
+  margin: 18px 0 30px;
+  font-style: italic;
+  color: black;
+`;
+
+export const Description = styled.p`
+  max-width: 500px;
+`;
+
+export const DetailedInfo = styled.div`
+  display: flex;
+`;
+
+export const DetailedInfoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 50px;
+`;
+
+export const Strong = styled.span`
+  font-weight: bold;
+  color: black;
+`;
+
+export const SmallInfo = styled.p`
+  margin: 0 8px;
+`;
+
+export const IngredientsWrapper = styled.div`
+  padding: 20px 50px;
+  margin-top: 30px;
+  box-shadow: rgba(186, 199, 213, 0.5) 0px 4px 12px;
+`;
