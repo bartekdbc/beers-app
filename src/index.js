2795b23c67affc2f164b27c9c3e73dee302254b6
Bartlomiej Debiec <116588815+bartekdbc@users.noreply.github.com>
2023-03-30 19:30:33 +0700

    small change with queryFn

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import App from "./App";
 import { GlobalStyle } from "./assets/GlobalStyle";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { fetchBeers } from "./beersAPI";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -13,7 +12,6 @@ const queryClient = new QueryClient({
     queries: {
       staleTime: 1000 * 10, // 10 seconds
       cacheTime: 1000 * 10, // 10 seconds
-      queryFn: fetchBeers,
     },
   },
 });
