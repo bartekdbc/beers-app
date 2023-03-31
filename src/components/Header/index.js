0d405977288976902526020edcaccc0969511620
Bartlomiej Debiec <116588815+bartekdbc@users.noreply.github.com>
2023-03-31 07:30:58 +0700

    Header updated

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,10 @@ import { LogoWrapper, StyledNavLink } from "./styled";
 const Header = () => {
   return (
     <LogoWrapper>
-      <StyledNavLink to={toBeerList()}>Beers</StyledNavLink>
+      <StyledNavLink to={toBeerList()}>
+        <img src="./beer256.png" alt="Beer Icon" />
+        <span>Beers</span>
+      </StyledNavLink>
     </LogoWrapper>
   );
 };
