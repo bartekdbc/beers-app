806e8f740e55327f720a8840f889bb2e8c46e1de
Bartlomiej Debiec <116588815+bartekdbc@users.noreply.github.com>
2023-04-01 16:35:28 +0700

    url display for page and per_page corrected

--- src/api/beersAPI.js
+++ src/api/beersAPI.js
@@ -1,10 +1,25 @@
 export const fetchBeers = async ({ queryKey }) => {
   const [, params] = queryKey;
 
-  const pageParams = new URLSearchParams(params);
-  const searchParams = new URLSearchParams(params);
+  const pageParams = new URLSearchParams();
+  const searchParams = new URLSearchParams();
 
-  const APIUrl = `https://api.punkapi.com/v2/beers?${pageParams}&${searchParams}`;
+  if (params.page) {
+    pageParams.set("page", params.page);
+  }
+  if (params.per_page) {
+    pageParams.set("per_page", params.per_page);
+  }
+
+  for (const [key, value] of Object.entries(params)) {
+    if (key !== "page" && key !== "per_page") {
+      searchParams.set(key, value);
+    }
+  }
+
+  const APIUrl = `https://api.punkapi.com/v2/beers${
+    pageParams.toString() ? `?${pageParams.toString()}` : ""
+  }${searchParams.toString() ? `&${searchParams.toString()}` : ""}`;
 
   const response = await fetch(APIUrl);
 
